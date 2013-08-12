a40bf74053f79a3f899364831e350045e503889e
Aurelian Jaggi <ajaggi@eaternity.ch>
2013-08-12 16:24:16 +0200

    Added Abbreviations to Unit

--- src/org/eaticious/common/Unit.java
+++ src/org/eaticious/common/Unit.java
@@ -1,42 +1,45 @@
 package org.eaticious.common;
-
+//APPROVE Sven schau dir die Erweiterung mit der Abkuerzung an / problem ist z.T. ist englisch und deutsch verschieden...
 public enum Unit {
 	
 	// base for mass is kilogram
-	TON(PhysicalDimension.MASS, 0.001), KILOGRAM(PhysicalDimension.MASS, 1d), METRIC_POUND(PhysicalDimension.MASS, 2d), GRAM(PhysicalDimension.MASS, 1000d), MILlIGRAM(PhysicalDimension.MASS, 1000000d), MIKROGRAM(PhysicalDimension.MASS, 1000000000d), 
-	POUND(PhysicalDimension.MASS, 2.2046226218488), OUNCE(PhysicalDimension.MASS, 35.27396194958), GRAIN(PhysicalDimension.MASS, 15432.358352941),
+	TON("t", PhysicalDimension.MASS, 0.001), KILOGRAM("kg", PhysicalDimension.MASS, 1d), METRIC_POUND("mlb", PhysicalDimension.MASS, 2d), GRAM("g", PhysicalDimension.MASS, 1000d), MILlIGRAM("mg", PhysicalDimension.MASS, 1000000d), MIKROGRAM("µg", PhysicalDimension.MASS, 1000000000d), 
+	POUND("lb", PhysicalDimension.MASS, 2.2046226218488), OUNCE("oz", PhysicalDimension.MASS, 35.27396194958), GRAIN("gr", PhysicalDimension.MASS, 15432.358352941),
 	
 	// base unit for volume is liter
-	HEKTOLITRE(PhysicalDimension.VOLUME, 0.01), 
-	LITRE(PhysicalDimension.VOLUME, 1d), 
-	DECILITRE(PhysicalDimension.VOLUME, 10d), 
-	CENTILITRE(PhysicalDimension.VOLUME, 100d),
-	BARREL(PhysicalDimension.VOLUME, 0.0062848352758387), 
-	GALLON_LIQUID(PhysicalDimension.VOLUME, 0.26417205235815), GALLON_DRY(PhysicalDimension.VOLUME, 0.22702074606721), 
-	CUP_LIQUID(PhysicalDimension.VOLUME, 4.2267528377304), CUP_DRY(PhysicalDimension.VOLUME, 3.6323319370754), 
-	GILL_LIQUID(PhysicalDimension.VOLUME, 8.4535056754607), GILL_DRY(PhysicalDimension.VOLUME, 7.2646638741508),
+	HEKTOLITRE("hl", PhysicalDimension.VOLUME, 0.01), 
+	LITRE("l", PhysicalDimension.VOLUME, 1d), 
+	DECILITRE("dl", PhysicalDimension.VOLUME, 10d), 
+	CENTILITRE("cl", PhysicalDimension.VOLUME, 100d),
+	BARREL("bbl", PhysicalDimension.VOLUME, 0.0062848352758387), 
+	GALLON_LIQUID("gal", PhysicalDimension.VOLUME, 0.26417205235815), GALLON_DRY("gal", PhysicalDimension.VOLUME, 0.22702074606721), 
+	CUP_LIQUID("cup", PhysicalDimension.VOLUME, 4.2267528377304), CUP_DRY("cup", PhysicalDimension.VOLUME, 3.6323319370754), 
+	GILL_LIQUID("gill", PhysicalDimension.VOLUME, 8.4535056754607), GILL_DRY("gill", PhysicalDimension.VOLUME, 7.2646638741508),
 	
-	TABLESPOON_LIQUID(PhysicalDimension.VOLUME, 67.6280454036864), TABLESPOON_DRY(PhysicalDimension.VOLUME, 58.1173109932064), 
-	TEASPOON_LIQUID(PhysicalDimension.VOLUME, 202.8841362110592), TEASPOON_DRY(PhysicalDimension.VOLUME, 174.3519329796192),
+	TABLESPOON_LIQUID("EL", PhysicalDimension.VOLUME, 67.6280454036864), TABLESPOON_DRY("EL", PhysicalDimension.VOLUME, 58.1173109932064), 
+	TEASPOON_LIQUID("TL", PhysicalDimension.VOLUME, 202.8841362110592), TEASPOON_DRY("TL", PhysicalDimension.VOLUME, 174.3519329796192),
 	
 	// base unit is kilometer 
-	KILOMETER(PhysicalDimension.DISTANCE, 1d), METER(PhysicalDimension.DISTANCE, 1000d),
-	YARD(PhysicalDimension.DISTANCE, 0.00091440), MILE(PhysicalDimension.DISTANCE, 1.6093),
-	NAUTICAL_MILE(PhysicalDimension.DISTANCE, 1.852),
+	KILOMETER("km", PhysicalDimension.DISTANCE, 1d), METER("m", PhysicalDimension.DISTANCE, 1000d),
+	YARD("yd", PhysicalDimension.DISTANCE, 0.00091440), MILE("mi", PhysicalDimension.DISTANCE, 1.6093),
+	NAUTICAL_MILE("nau mi", PhysicalDimension.DISTANCE, 1.852),
 	
 	// base unit for energy is kWh
-	KILOWATTHOUR(PhysicalDimension.ENERGY, 1d), 
-	WATTHOUR(PhysicalDimension.ENERGY, 1000d), 
-	JOULE(PhysicalDimension.ENERGY, 3600000d), 
-	KILOCALORIES(PhysicalDimension.ENERGY, 859.84522785899),
-	BRITISH_THERMAL_UNIT(PhysicalDimension.ENERGY, 3412.1416331279),
+	KILOWATTHOUR("KWh",PhysicalDimension.ENERGY, 1d), 
+	WATTHOUR("Wh", PhysicalDimension.ENERGY, 1000d), 
+	JOULE("J", PhysicalDimension.ENERGY, 3600000d), 
+	KILOCALORIES("kcal", PhysicalDimension.ENERGY, 859.84522785899),
+	BRITISH_THERMAL_UNIT("BTU", PhysicalDimension.ENERGY, 3412.1416331279),
 	
-	CO2E(PhysicalDimension.WARMING_POTENTIAL, 1d), CO2(PhysicalDimension.WARMING_POTENTIAL, 1d), CH4(PhysicalDimension.WARMING_POTENTIAL, 25d), N2O(PhysicalDimension.WARMING_POTENTIAL, 298d),
+	CO2E("CO2e", PhysicalDimension.WARMING_POTENTIAL, 1d), CO2("CO2", PhysicalDimension.WARMING_POTENTIAL, 1d), CH4("CH4", PhysicalDimension.WARMING_POTENTIAL, 25d), N2O("N2O", PhysicalDimension.WARMING_POTENTIAL, 298d),
 	
-	KG_CO2E(PhysicalDimension.CO2E, 1d), G_CO2E(PhysicalDimension.CO2E, 1000d), T_CO2E(PhysicalDimension.CO2E, 0.001),
+	KG_CO2E("kg CO2e", PhysicalDimension.CO2E, 1d), G_CO2E("g CO2e", PhysicalDimension.CO2E, 1000d), T_CO2E("t CO2e", PhysicalDimension.CO2E, 0.001),
 	
 	// these are not really physical units, just used in recipes. We might want to handle them differently
-	PIECE(PhysicalDimension.OTHER, 0d), SLICE(PhysicalDimension.OTHER, 0d), NONE(PhysicalDimension.OTHER, 0d);
+	PIECE("piece", PhysicalDimension.OTHER, 0d), SLICE("slice", PhysicalDimension.OTHER, 0d), NONE("", PhysicalDimension.OTHER, 0d);
+	
+	// The short version of the measuring unit for display
+	private String abbreviation;
 	
 	// The physical dimension measured with this unit, e.g. mass, volume, energy
 	private PhysicalDimension dimension;
@@ -49,9 +52,10 @@ public enum Unit {
 	 * @param dimension The dimension used (hardcoded as String)
 	 * @param factor The conversion factor to convert into the base unit
 	 */
-	private Unit(final PhysicalDimension dimension, final Double factor){
+	private Unit(String abbreviation, final PhysicalDimension dimension, final Double factor){
 		this.dimension = dimension;
 		this.factor = factor;
+		this.abbreviation = abbreviation;
 	}
 	
 	/**
@@ -60,6 +64,14 @@ public enum Unit {
 	 */
 	public PhysicalDimension getDimension(){
 		return this.dimension;
+	}
+	
+	/**
+	 * 
+	 * @return The short version of the measuring unit for display 
+	 */
+	public String getAbbreviation() {
+		return this.abbreviation;
 	}
 	
 	/**
